d8a708ff907285cb4789c94d58d08c843fcf47cc
Duino-nano <wsdqr907869@yahoo.co.jp>
2024-07-19 21:46:41 +0900

    コード整理

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ const int upKey    = 0;
 const int resetKey = 1;
 const int startKey = 2;
 // 明星チャルメラの曲の音階
-int melody[] = {
+const int melody[] = {
     // ド   レ   ミー レ    ド　    ド    レ   ミ    レ  ド   レ　
     0, 523, 587, 659, 587, 523, 0, 523, 587, 659, 587, 523, 587, 0,
 };
 // 明星チャルメラの曲の音の長さ
-int noteDurations[] = {
+const int noteDurations[] = {
     1000, 150, 150, 450, 150, 150, 450, 150, 150, 150, 150, 150, 900, 1000,
 };
 
@@ -40,14 +40,15 @@ int segDot[] = {0, 0, 0};  // ドット表示用の配列
 
 const int buttonPin[3] = {upButtonPin, resetButtonPin, startButtonPin};  // ボタンのピン
 
-// 表示する数字
+// 表示する数字　ここに表示したい数字を入れる
 int dispNum = 0;
 
+// 関数の宣言
 void displaydispNumWithTimer();
 void buzzer(int stopFlg);
 int getKey(int key);
 
-// セットアップ
+// セットアップ　最初に一度だけ実行される
 void setup() {
   // セグメント表示用のピンを出力に設定
   for (int i = 0; i < (sizeof(segmentPins) / sizeof(segmentPins[0])); i++) {
@@ -76,7 +77,7 @@ void setup() {
   Serial.begin(9600);
 }
 
-// メイン処理
+// メイン処理　繰り返し実行される
 void loop() {
   static unsigned long oldTime = millis();
   static int countTime         = 0;
@@ -107,23 +108,24 @@ void loop() {
   }
 
   // カウントダウン処理
-
   if (millis() - oldTime > 100) {
     oldTime = millis();
-    if (startFlg == 1 && countTime > 0) {
-      countTime--;
-      segDot[0] = countTime % 2;
+    if (startFlg == 1 && countTime > 0) {  // 　カウント中の処理
+      countTime--;                         // 1秒ずつ減らす
+      segDot[0] = countTime % 2;           // 1秒毎にドットを点滅
     } else {
-      segDot[0] = 0;
+      segDot[0] = 0;  // カウント中でない場合はドットを消灯
     }
   }
 
+  // 表示数の処理
   if (countTime >= 60) {  // 60秒以上の場合は分に変換
     dispNum = countTime / 60;
   } else {
     dispNum = countTime;
   }
 
+  // カウントダウンが0になったらブザーを鳴らす
   if (countTime == 0 && startFlg == 1) {
     buzzer(0);
   } else {
@@ -131,43 +133,48 @@ void loop() {
   }
 }
 
+// ここから下は関数の定義
+
+// ブザーを鳴らす関数　stopFlgが1の場合はブザーを止める
 void buzzer(int stopFlg) {
-  static int thisNote           = 0;
-  static unsigned long oldTime  = millis();
-  static unsigned long waitTime = 0;
-  if (stopFlg == 1) {
+  static int thisNote           = 0;         // 今の音階
+  static unsigned long oldTime  = millis();  // 時間計測用のバッファ
+  static unsigned long waitTime = 0;         // 次の音階までの時間
+  if (stopFlg == 1) {                        // stopFlgが1の場合はブザーを止めて初期化
     thisNote = 0;
     waitTime = 0;
     oldTime  = millis();
-    noTone(buzzerPin);
-    return;
+    noTone(buzzerPin);  // ブザーを止める
+    return;             // 関数を抜ける
   }
-  if (millis() - oldTime > waitTime) {
-    oldTime = millis();
-    if (thisNote < (sizeof(melody) / sizeof(melody[0]))) {
-      waitTime = noteDurations[thisNote];
-      tone(buzzerPin, melody[thisNote], noteDurations[thisNote]);
-      thisNote++;
+  if (millis() - oldTime > waitTime) {                             // 次の音階までの時間が経過したら次の音階を鳴らす
+    oldTime = millis();                                            // 時間をリセット
+    if (thisNote < (sizeof(melody) / sizeof(melody[0]))) {         // 最後の音階まで行ったら初期化
+      waitTime = noteDurations[thisNote];                          // 次の音階までの時間を設定
+      tone(buzzerPin, melody[thisNote], noteDurations[thisNote]);  // 音階を鳴らす
+      thisNote++;                                                  // 次の音階に進む
     } else {
       thisNote = 0;
     }
   }
 }
 
+// セグメント表示関数　dispNumに表示したい数字を入れる　segには表示する桁を入れる
 void displaydispNum(int dispNum, int seg) {
   bool pinState = false;
-  for (int i = 0; i < (sizeof(segmentPins) / sizeof(segmentPins[0])); i++) {
-    pinState = bool(segment[dispNum] & (1 << i));  // 1ビットずつシフトして表示するかどうかを判定
-    digitalWrite(segmentPins[i], pinState);
-    if (i == 7) {  // 8番目のピンにはドットを表示
-      digitalWrite(segmentPins[i], segDot[seg]);
+  for (int i = 0; i < (sizeof(segmentPins) / sizeof(segmentPins[0])); i++) {  // セグメントの各ピンに対して処理
+    pinState = bool(segment[dispNum] & (1 << i));                             // 1ビットずつシフトして表示するかどうかを判定
+    digitalWrite(segmentPins[i], pinState);                                   // ピンの状態を設定
+    if (i == 7) {                                                             // 8番目のピンにはドットを表示
+      digitalWrite(segmentPins[i], segDot[seg]);                              // ドット表示
     }
   }
 }
 
+// 桁表示関数　桁を指定して表示する　0が1の位　1が10の位
 void displayDigit(int digit) {
-  for (int i = 0; i < (sizeof(digitPins) / sizeof(digitPins[0])); i++) {
-    if (i == digit) {
+  for (int i = 0; i < (sizeof(digitPins) / sizeof(digitPins[0])); i++) {  // 桁の各ピンに対して処理
+    if (i == digit) {                                                     // 表示する桁のピンをHIGHにする
       digitalWrite(digitPins[i], HIGH);
     } else {
       digitalWrite(digitPins[i], LOW);
@@ -175,12 +182,13 @@ void displayDigit(int digit) {
   }
 }
 
-// タイマー割込みを使用したセグメント表示delayは使わない
+// タイマー割込みから呼ばれるセグメント表示関数　1msごとに呼ばれる
 void displaydispNumWithTimer() {
   static int digit = 0;
-  static int onoff = 1;
-  int num[]        = {0, 0};
-  // 桁分け
+  static int onoff = 1;       // セグメントを消灯するかどうかのフラグ
+  int num[]        = {0, 0};  // 表示する数字を桁分けしたものを入れる配列
+
+  // 桁分け 1の位と10の位に分ける
   if (dispNum < 0) {  // 0未満の場合は0にする
     dispNum = 0;
     num[0]  = 0;
@@ -190,27 +198,28 @@ void displaydispNumWithTimer() {
     num[1] = dispNum / 10;  // 10の位
   }
 
-  if (digit == 0) {  // 1の位の処理
-    if (onoff == 1) {
-      displayDigit(0);
-      displaydispNum(num[0], 0);
-      onoff = 0;
+  // 1msごとに呼ばれるので1msごとに表示処理をする。一桁目表示→消灯→二桁目表示→消灯の順で表示(消灯は残像対策)
+  if (digit == 0) {               // 1の位の処理
+    if (onoff == 1) {             // 1の場合セグメントを表示
+      displayDigit(0);            // 1の位の桁を表示
+      displaydispNum(num[0], 0);  // 1の位の数字を表示
+      onoff = 0;                  // 次の割り込みで消灯するようにする
     } else {
       displayDigit(3);        // 消灯
       displaydispNum(10, 2);  // 消灯
-      onoff = 1;
-      digit = 1;
+      onoff = 1;              // 次の割り込みで表示するようにする
+      digit = 1;              // 10の位の処理に移る
     }
   } else if (digit == 1) {            // 10の位の処理
     if (onoff == 1 && num[1] != 0) {  // 10の位が0の場合は表示しない
-      displayDigit(1);
-      displaydispNum(num[1], 1);
-      onoff = 0;
+      displayDigit(1);                // 10の位の桁を表示
+      displaydispNum(num[1], 1);      // 10の位の数字を表示
+      onoff = 0;                      // 次の割り込みで消灯するようにする
     } else {
       displayDigit(3);        // 消灯
       displaydispNum(10, 2);  // 消灯
-      onoff = 1;
-      digit = 0;
+      onoff = 1;              // 次の割り込みで表示するようにする
+      digit = 0;              // 1の位の処理に移る
     }
   }
 }
@@ -219,12 +228,12 @@ void displaydispNumWithTimer() {
 int getKey(int key) {
   static int oldState[3] = {HIGH, HIGH, HIGH};           // 前のボタンの状態入れる配列
   int newState           = digitalRead(buttonPin[key]);  // ボタンの状態を取得
-  int returnState        = 0;
-  if (oldState[key] == HIGH && newState == LOW) {  // ボタンが押された瞬間(HIGH -> LOWになった瞬間)
-    returnState = 1;
+  int returnState        = 0;                            // 戻り値
+  if (oldState[key] == HIGH && newState == LOW) {        // ボタンが押された瞬間(HIGH -> LOWになった瞬間)
+    returnState = 1;                                     // 戻り値を1にする
   } else {
-    returnState = 0;
+    returnState = 0;  // ボタンが押された瞬間以外は戻り値を0にする
   }
   oldState[key] = newState;  // 状態を保存
-  return returnState;
+  return returnState;        // 戻り値を返す
 }
